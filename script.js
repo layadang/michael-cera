ed92b534d196742261c7757d9257cc3882744404
layadang <pd03@bu.edu>
2023-07-28 09:14:10 +0000

    yassified it (added sparkly cursor and made everything pink)

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var generateButton = document.getElementById('generate-btn');
-var buttonText = 'Generate';
+var buttonText = 'see mikey boy now!';
 var clickCount = 0;
+var usedIndices = [];
 
 function generateRandomImage() {
+  var imageUrlElement = document.getElementById('michael-cera-image');
+  var imageContainer = document.getElementById('image-container');
+
+  imageUrlElement.style.opacity = 0;
+
+  // silly alerts
   if (clickCount == 1) {
     buttonText = 'another one?';
   }
@@ -20,6 +27,7 @@ function generateRandomImage() {
     alert('stop it.');
   }
 
+  // generating random image
   var bucketName = 'testing-michael-cera';
   var apiUrl = 'https://www.googleapis.com/storage/v1/b/' + bucketName + '/o';
 
@@ -27,12 +35,24 @@ function generateRandomImage() {
     .then(response => response.json())
     .then(data => {
       var objects = data.items;
-      var randomIndex = Math.floor(Math.random() * objects.length);
-      var imageUrlElement = document.getElementById('michael-cera-image');
-      imageUrlElement.src = objects[randomIndex].mediaLink;
-      var imageContainer = document.getElementById('image-container');
-      imageContainer.style.display = 'block';
-      generateButton.textContent = buttonText;
+      var availableIndices = objects.map((_, index) => index).filter(index => !usedIndices.includes(index));
+
+      if (availableIndices.length === 0) {
+        // reset after all images shown
+        usedIndices = [];
+        return;
+      }
+
+      var randomIndex = availableIndices[Math.floor(Math.random() * availableIndices.length)];
+      var newImage = new Image();
+      newImage.onload = function() {
+        imageUrlElement.src = this.src;
+        imageUrlElement.style.opacity = 1;
+        imageContainer.style.display = 'block';
+        generateButton.textContent = buttonText;
+        usedIndices.push(randomIndex);
+      };
+      newImage.src = objects[randomIndex].mediaLink; // Start loading the new image
     })
     .catch(error => {
       console.log('An error occurred:', error);
